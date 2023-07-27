2dcc551557e6028c735c9f5e3880762add2740ee
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-07-27 05:11:35 +0000

    Debug

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 export default getResponseFromAPI() {
-  const success = True;
-  return new Promise ((resolve, reject) => {
+  const success = true;
+  return new Promise((resolve, reject) => {
     if (success) {
       resolve();
     } else {
       reject();
     }
- )}
+  });
 }
