ec3a31257c9dc0dd734844b4682b6e248a16f5e1
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-08-01 05:16:20 +0000

    Add task1

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-  object1 = {id: '1', firstName: 'Guillaume', location: 'San Francisco'}
-  object2 = {id: '2', firstName: 'James', location: 'Columbia'}
-  object3 = {id: '5', firstName: 'San Francisco'}
-  arr = new array(object1, object2, object3);
-  return (arr);
+return [
+  {id: '1', firstName: 'Guillaume', location: 'San Francisco'},
+  {id: '2', firstName: 'James', location: 'Columbia'},
+  {id: '5', firstName: 'San Francisco'}
+  ]
 }
