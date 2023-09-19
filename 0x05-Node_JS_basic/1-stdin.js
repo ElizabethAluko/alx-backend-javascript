cd020b1c6d98ce948c03269b53503d93c5c09ac6
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-09-19 10:17:42 +0000

    Add task

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+
+// Create an interface for reading from the command line
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Display the initial message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Listen for user input
+rl.on('line', (input) => {
+  // Display the user's name
+  console.log(`Your name is: ${input}`);
+
+  // Close the program
+  console.log('This important software is now closing');
+  rl.close();
+});
