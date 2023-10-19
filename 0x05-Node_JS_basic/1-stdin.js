3764064fead491e64d3f97c3f7cead86302f569a
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-10-19 09:56:25 +0100

    Add task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,3 @@
-//Using Process stdin
-
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -7,21 +5,14 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-if (process.argv.length === 3) {
-  const name = process.argv[2];
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit(0);
-} else {
-  console.log('Welcome to Holberton School, what is your name?');
-
-  rl.question('', (name) => {
+rl.question('', (name) => {
+  if (name) {
     console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    rl.close();
-  });
+  }
+  console.log('This important software is now closing');
+  rl.close();
+});
 
-  rl.on('close', () => {
-    process.exit(0);
-  });
-}
+rl.on('close', () => {
+  process.exit(0);
+});
