b578c639b72f7e22bbde48cbf5e5ea1ba1539058
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-10-19 09:52:22 +0100

    Add task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,27 @@
-//1-stdin module
+//Using Process stdin
 
 const readline = require('readline');
 
-// Create an interface for reading from the command line
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-// Display the initial message
-console.log('Welcome to Holberton School, what is your name?');
+if (process.argv.length === 3) {
+  const name = process.argv[2];
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+  process.exit(0);
+} else {
+  console.log('Welcome to Holberton School, what is your name?');
 
-// Listen for user input
-rl.on('line', (input) => {
-  // Display the user's name
-  console.log(`Your name is: ${input}`);
+  rl.question('', (name) => {
+    console.log(`Your name is: ${name}`);
+    console.log('This important software is now closing');
+    rl.close();
+  });
 
-  // Close the program
-  if (process.stdin.isTTY) {
-	  rl.close();
-  } else {
-	  console.log('This important software is now closing');
-	  rl.close();
-  }
-});
+  rl.on('close', () => {
+    process.exit(0);
+  });
+}
