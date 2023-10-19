1f1413f188fa0a621548059f14595744015d820c
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-10-19 10:20:25 +0100

    Add task

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.trim().split('\n');
+    let students = 0;
+    const fields = {};
+
+    for (const line of lines) {
+      const studentData = line.split(',');
+      if (studentData.length === 2) {
+        students++;
+        const field = studentData[0].trim();
+        const name = studentData[1].trim();
+        if (fields[field] === undefined) {
+          fields[field] = [];
+        }
+        fields[field].push(name);
+      }
+    }
+
+    console.log(`Number of students: ${students}`);
+
+    for (const field in fields) {
+      const studentCount = fields[field].length;
+      const studentList = fields[field].join(', ');
+      console.log(`Number of students in ${field}: ${studentCount}. List: ${studentList}`);
+    }
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.error('Cannot load the database');
+    } else {
+      throw error;
+    }
+  }
+}
